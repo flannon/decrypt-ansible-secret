f4129cff592af66348dca97797fd080f1f554b93
Flannon Jackson <flannon@nyu.edu>
2018-04-29 10:55:22 -0400

    Cleaned up commentary and debugging

--- avdecrypt.py
+++ avdecrypt.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+# References used for this descriptor
+#  I barrowed heavily from andrunah's solution for decrypting
+#  ansible vaulted variables, available at
+#    https://github.com/andrunah/ansible-vault-variable-updater
+#  As well as the following material
+#    https://github.com/ansible/ansible/issues/26190
+#    https://stackoverfow.com/a/15015748/3447107
+#    https://security.openstack.org/guidelines/dg_apply-restrictive-file-permissions.html
+#
 from __future__ import print_function
 
 import codecs
@@ -8,11 +17,8 @@ from os.path import expanduser
 from pwd import getpwnam
 import stat
 import sys
-import yaml
 from ansible.parsing import vault
 from ansible.parsing.dataloader import DataLoader
-from ansible.parsing.yaml import objects
-from ansible.parsing.yaml.dumper import AnsibleDumper
 from ansible.parsing.yaml.loader import AnsibleLoader
 
 
@@ -35,7 +41,7 @@ def init():
         sys.exit(1)
 
     # Check that directory and file permissions are set properly
-    if (sduid != uid or sdperm != 16832 or sfperm != 33152):
+    if (sduid != uid or sfuid != uid or sdperm != 16832 or sfperm != 33152):
         print (secrets_dir_path, "is misconfigured.")
         sys.exit(2)
 
@@ -47,7 +53,6 @@ def decrypt(secrets_file_path, decrypted_file_path):
     try:
         vault_password_file = os.environ["ANSIBLE_VAULT_PASSWORD_FILE"]
     except KeyError:
-        #print ("export ANSIBLE_VAULT_PASSWORD_FILE")
         sys.exit(3)
 
     with open(vault_password_file, 'r') as vpf:
@@ -64,19 +69,10 @@ def decrypt(secrets_file_path, decrypted_file_path):
     with codecs.open(secrets_file_path, 'r', encoding='utf-8') as f:
         loaded_yaml = AnsibleLoader(f, vault_secrets=_vault.secrets).get_single_data()
 
-    # Modify yml with new encrypted values
-    new_encrypted_variable = objects.AnsibleVaultEncryptedUnicode \
-        .from_plaintext(vault_password, _vault, vault_secrets[0][1])
-
-    # Write a new encrypted yml
-    with open('new_variables.yml','w') as fd:
-        yaml.dump(loaded_yaml, fd, Dumper=AnsibleDumper, encoding=None, default_flow_style=False)
-
-
     # Define decrypted file params
-    open_flags = (os.O_CREAT | os.O_EXCL | os.O_WRONLY)
-    open_mode = stat.S_IRUSR | stat.S_IWUSR # 0o600 in octal
-    open_umask = os.umask(0) # Save current umask to prevent downgrading to 0
+    flags = (os.O_CREAT | os.O_EXCL | os.O_WRONLY)
+    mode = stat.S_IRUSR | stat.S_IWUSR  # 0o600 in octal
+    umask = os.umask(0)  # Save current umask to prevent downgrading to 0
 
     # Delete and replace decrypted secrets to ensure file permissions
     try:
@@ -85,16 +81,15 @@ def decrypt(secrets_file_path, decrypted_file_path):
         pass
 
     # Open the file descriptor
-    umask_original = os.umask(open_umask)
+    umask_original = os.umask(umask)
     try:
-        decrypted_file_fd = os.open(decrypted_file_path, open_flags, open_mode)
+        decrypted_file_fd = os.open(decrypted_file_path, flags, mode)
     finally:
         os.umask(umask_original)
-        #os.umask(0o777)
 
     # Open file handle and write the decrypted file
     decrypted_file_out = os.fdopen(decrypted_file_fd, 'w')
-    for k,v in loaded_yaml.items():
+    for k, v in loaded_yaml.items():
         line = "export " + str(k) + "=" + str(v) + "\n"
         decrypted_file_out.write(line)
     decrypted_file_out.close()

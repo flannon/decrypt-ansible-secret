b47fce8392c8e7fb908e6052013092b09820001a
Flannon Jackson <flannon@nyu.edu>
2018-04-29 10:27:47 -0400

    Updated avdecrypt.py to use fdopen

--- avdecrypt.py
+++ avdecrypt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from __future__ import print_function
+
 import codecs
 import getpass
 import os
@@ -17,46 +19,39 @@ from ansible.parsing.yaml.loader import AnsibleLoader
 def init():
 
     try:
-        #sdir = "./test"
-        sdir = expanduser("~") + "/.secrets"
-        sfile = sdir + "/secrets.yml"
-        dfile = sdir + "/decrypted"
-        #sfile = sdir + "/secrets"
+        secrets_dir_path = expanduser("~") + "/.ansible-vault-secrets"
+        secrets_file_path = secrets_dir_path + "/secrets.yml"
+        decrypted_file_path = secrets_dir_path + "/decrypted"
         user = getpass.getuser()
         uid = getpwnam(user).pw_uid
-        sdstat = os.stat(sdir)
-        sfstat = os.stat(sfile)
+        sdstat = os.stat(secrets_dir_path)
+        sfstat = os.stat(secrets_file_path)
+        sduid = sdstat.st_uid
+        sfuid = sfstat.st_uid
+        sdperm = sdstat.st_mode
+        sfperm = sfstat.st_mode
     except KeyError:
-        print ".secrets bad permissions"
+        print (".secrets imporperly configured")
         sys.exit(1)
 
-    sduid = sdstat.st_uid
-    sfuid = sfstat.st_uid
-    sdperm = sdstat.st_mode
-    sfperm = sfstat.st_mode
+    # Check that directory and file permissions are set properly
+    if (sduid != uid or sdperm != 16832 or sfperm != 33152):
+        print (secrets_dir_path, "is misconfigured.")
+        sys.exit(2)
 
-    #if check file perms
-
-    print "sdperm: " + str(sdperm) + " sfperm: " + str(sfperm)
-    print "sduid: " + str(sduid)
-
-    return sfile, dfile
+    return secrets_file_path, decrypted_file_path
 
 
-def decrypt(secrets_file, decrypted_file):
-
-    #secrets_file = expanduser("~") + "/.secrets/secrets.yml"
+def decrypt(secrets_file_path, decrypted_file_path):
 
     try:
         vault_password_file = os.environ["ANSIBLE_VAULT_PASSWORD_FILE"]
     except KeyError:
-        print "export ANSIBLE_VAULT_PASSWORD_FILE"
-        sys.exit(2)
+        #print ("export ANSIBLE_VAULT_PASSWORD_FILE")
+        sys.exit(3)
 
     with open(vault_password_file, 'r') as vpf:
         vault_password = vpf.read().replace('\n', '')
-
-    print "vault_passwordi: " + vault_password
 
     # Load vault password and prepare secrets for decryption
     loader = DataLoader()
@@ -66,7 +61,7 @@ def decrypt(secrets_file, decrypted_file):
     _vault = vault.VaultLib(vault_secrets)
 
     # Load encrypted yml for processing
-    with codecs.open(secrets_file, 'r', encoding='utf-8') as f:
+    with codecs.open(secrets_file_path, 'r', encoding='utf-8') as f:
         loaded_yaml = AnsibleLoader(f, vault_secrets=_vault.secrets).get_single_data()
 
     # Modify yml with new encrypted values
@@ -77,26 +72,36 @@ def decrypt(secrets_file, decrypted_file):
     with open('new_variables.yml','w') as fd:
         yaml.dump(loaded_yaml, fd, Dumper=AnsibleDumper, encoding=None, default_flow_style=False)
 
-    #print(loaded_yaml)
-    #print type(loaded_yaml)
 
-    sf_out = open(decrypted_file, "w")
+    # Define decrypted file params
+    open_flags = (os.O_CREAT | os.O_EXCL | os.O_WRONLY)
+    open_mode = stat.S_IRUSR | stat.S_IWUSR # 0o600 in octal
+    open_umask = os.umask(0) # Save current umask to prevent downgrading to 0
+
+    # Delete and replace decrypted secrets to ensure file permissions
+    try:
+        os.remove(decrypted_file_path)
+    except OSError:
+        pass
+
+    # Open the file descriptor
+    umask_original = os.umask(open_umask)
+    try:
+        decrypted_file_fd = os.open(decrypted_file_path, open_flags, open_mode)
+    finally:
+        os.umask(umask_original)
+        #os.umask(0o777)
+
+    # Open file handle and write the decrypted file
+    decrypted_file_out = os.fdopen(decrypted_file_fd, 'w')
     for k,v in loaded_yaml.items():
         line = "export " + str(k) + "=" + str(v) + "\n"
-        print line
-        #print "type k:", type(k)
-        #print "Key: ", key
-        sf_out.write(line)
-        #print k,":", v
-        #os.environ[k] = str(v)
-        #os.putenv(k, str(v))
-    sf_out.close()
+        decrypted_file_out.write(line)
+    decrypted_file_out.close()
 
 
 def main():
     secrets_file, decrypted_file = init()
-    print secrets_file
-    print type(secrets_file)
     decrypt(secrets_file, decrypted_file)
 
 

59a46f6f7dd8b256964fb99c3463b249c93a5a18
Flannon Jackson <flannon@nyu.edu>
2018-05-09 22:06:59 -0400

    Initial commit

---
+++ dotaws/cmk_arn.sh
@@ -0,0 +1,2 @@
+export CMK_AWS_ENCRYPTION_SDK="arn:aws:kms:us-east-1:111111111111:key/feb294ed-07f5-5644-86e3-d7c296e1fdfw"
+export CMK_ARN="arn:aws:kms:us-east-1:111111111111:key/feb294ed-07f5-5644-86e3-d7c296e1fdfw"
